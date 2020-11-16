aa47e3b84297aa6aee13a7ea626c9f95c7e6db68
Aman Gupta <amangupta6121998@gmail.com>
2020-11-16 16:19:50 +0530

    Create Excercise-11.js

---
+++ week-01 First Program/Excercise-11.js
@@ -0,0 +1,11 @@
+//-------------Excersise -11 : print every item on the list--------------------
+
+
+var grocery = ["Bread","Milk","Pasta","Egg","Oil"];
+
+for(var i=0;i<=grocery.length;i++)
+{
+   
+      console.log(grocery[i]);
+    
+}
