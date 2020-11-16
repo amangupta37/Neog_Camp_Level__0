b1d2eeadcaa6a2a9d078862d9762bc4029fc2c1f
Aman Gupta <amangupta6121998@gmail.com>
2020-11-16 16:04:59 +0530

    Update Excercise-09(homework).js

--- week-01 First Program/Excercise-09(homework).js
+++ week-01 First Program/Excercise-09(homework).js
@@ -1,1 +1,21 @@
+//--------------------------Pyramid Pattern Printing in CLI-------------------------------
+var num = "";
+var size = 5;
+for(var i=1; i<=size; i++)
+{
+  num = num + "*";
+  console.log(num);
+}
 
+
+//-------------------------Pyramid Pattern Printing in webpage-----------------------------
+
+for(var i=0;i<5;i++)
+{
+    for(var j=0;j<=i;j++)
+    {
+        document.write("*");
+         
+    }
+   document.write("<br>");
+}
