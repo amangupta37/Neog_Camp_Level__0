1bdddd71867e2ff253685d485508dc2669d2a317
Aman Gupta <amangupta6121998@gmail.com>
2020-11-16 20:17:39 +0530

    Update Excercise-12.js

--- week-01 First Program/Excercise-12.js
+++ week-01 First Program/Excercise-12.js
@@ -1,1 +1,25 @@
+//-------------Excersise -12 : club info about a superhero together--------------------
+var spiderman = 
+{
+    name :"PeterParker",
+    power: "web-shooter",
+    skin_color: "redandblue",
+    strength : 70,
+    intelligence :100
+}
 
+console.log(spiderman.name);
+console.log(spiderman.power);
+console.log(spiderman.strength);
+
+var joker =
+{
+    name :"Jack Napier",
+    power :"Shapeshifting",
+    skin_color:"white",
+    strength:50,
+    intelligence:200
+}
+console.log(joker.name);
+console.log(joker.power);
+console.log(joker.strength);
