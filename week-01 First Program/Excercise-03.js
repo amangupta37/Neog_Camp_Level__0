ed067749d31c823e29e55eabc240db621ccb62f2
Aman Gupta <amangupta6121998@gmail.com>
2020-11-16 10:09:12 +0530

    Create Excercise-03.js

---
+++ week-01 First Program/Excercise-03.js
@@ -0,0 +1,9 @@
+//-------------Excersise -03 : welcome your user--------------------
+
+var input = require('readline-sync');
+
+var username =input.question("Enter Your Name");
+
+var msg = "Welcome";
+
+console.log(username + " " + msg);
