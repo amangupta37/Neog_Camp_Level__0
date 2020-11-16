e3601b61d107fee7adf9ae1692f70fe73fc0f06b
Aman Gupta <amangupta6121998@gmail.com>
2020-11-16 10:17:03 +0530

    Create Excercise-04.js

---
+++ week-01 First Program/Excercise-04.js
@@ -0,0 +1,9 @@
+//-------------Excersise -04 : do it all together--------------------
+var input = require('readline-sync');
+
+var username =input.question("Enter Your Name");
+
+var msg = "Welcome";
+
+console.log(username);
+console.log(msg);
