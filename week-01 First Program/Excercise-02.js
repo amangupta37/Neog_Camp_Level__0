b5a38ddc7e4b0fdf56fcf326b026d79742f40518
Aman Gupta <amangupta6121998@gmail.com>
2020-11-16 10:08:25 +0530

    Create Excercise-02.js

---
+++ week-01 First Program/Excercise-02.js
@@ -0,0 +1,7 @@
+//-------------Excersise -02 :read the name of your user--------------------
+
+var input = require('readline-sync');
+
+var username =input.question("Enter Your Name");
+
+console.log(username);
